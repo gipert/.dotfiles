ce87c45e8386a5bdbb220f73846e96655a7f52c8
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-03-19 14:56:56 +0100

    [i3] update startup settings

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,18 +1,20 @@
 i3-msg 'workspace 8:VIII; append_layout /home/pertoldi/.config/i3/layouts/workspace-8.json'
 i3-msg 'workspace 9:IX; append_layout /home/pertoldi/.config/i3/layouts/workspace-9.json'
 i3-msg 'workspace 10:X; append_layout /home/pertoldi/.config/i3/layouts/workspace-10.json'
-#i3-msg 'workspace 1:I;  append_layout /home/pertoldi/.config/i3/layouts/workspace-1.json'
 i3-msg 'workspace 1:I'
 
-st -n htop -e htop &
-st -n vtop -e vtop --theme gruvbox &
-st -n ncmpcpp -e ncmpcpp &
-st -n vis -e vis &
+alacritty -t htop -e htop &
+alacritty -t vtop -e vtop --theme gruvbox &
+alacritty -t ncmpcpp -e ncmpcpp &
+alacritty -t vis -e vis &
+alacritty -t vis -e vis &
+alacritty -t vis -e vis &
 
-#nohup st -n st-256color &
-
-vivaldi --new-window github.com/gipert &
+google-chrome --new-window &
 sleep 0.5
 
+google-chrome --new-window web.whatsapp.com &
 Telegram &
-vivaldi --new-window web.whatsapp.com &
+sleep 1.0
+
+alacritty &
