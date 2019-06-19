884d5a83e1c2b42bbb8efd63fed56d47772f492b
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-06-19 19:20:29 +0200

    [root] add rootlogon.C and rootrc

---
+++ .rootlogon.C
@@ -0,0 +1,8 @@
+{
+    // nicer color map?
+    gStyle->SetPalette(kViridis);
+
+    // do not display any of the standard histogram decorations
+    gStyle->SetOptStat(false);
+    gStyle->SetOptFit(0);
+}
