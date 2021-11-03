1891c74f34cf3bc85f46520e0822304fc7bdce93
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2021-11-03 10:30:34 +0100

    Remove rocketchat from autostart, update dunst settings

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apps=(thunderbird telegram-desktop whatsapp-nativefier slack rocketchat-desktop firefox)
+apps=(thunderbird telegram-desktop whatsapp-nativefier slack firefox)
 
 for app in ${apps[@]}; do
     if [[ "$app" == "whatsapp-nativefier" ]]; then
-        killall WhatsApp > /dev/null
+        killall WhatsApp &> /dev/null
     else
-        killall -I $app > /dev/null
+        killall -I $app &> /dev/null
     fi
     exec $app > /dev/null &
 done
